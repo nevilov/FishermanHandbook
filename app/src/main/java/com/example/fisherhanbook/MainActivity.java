967e872c52cf6ade0c191813ff3a1f796007fdbf
Alim Nevilov <52937912+nevilov@users.noreply.github.com>
2020-08-02 14:10:08 +0300

    V-0.1

---
+++ app/src/main/java/com/example/fisherhanbook/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.fisherhanbook;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    private DrawerLayout drawer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView.setNavigationItemSelectedListener(this); // Добавляем в меню кнопку
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) { //Слушитель нажатий
+        int id = item.getItemId();
+        if(id == R.id.nav_home){
+            Toast.makeText(this, "Привет, домой", Toast.LENGTH_SHORT).show();
+
+        }
+        else if(id == R.id.nav_gallery)
+        {
+            Toast.makeText(this,  "Привет, галерея", Toast.LENGTH_SHORT).show();
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
