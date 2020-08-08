831f6d6c81a34f4577cbbc1c14f2539c0c4833b2
Alim Nevilov <52937912+nevilov@users.noreply.github.com>
2020-08-08 21:55:05 +0300

    V-0.6 (BETA Animation & Fix Buggs

--- app/src/main/java/com/example/fisherhanbook/StartActivity.java
+++ app/src/main/java/com/example/fisherhanbook/StartActivity.java
@@ -4,15 +4,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 
 public class StartActivity extends Activity {
+    private Animation logoApplication, buttonApplication;
+    private Button buttonAnimation;
+    private ImageView logoImage;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        init();
+
+    }
+
+    private void init(){
+        logoApplication = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.logo_animation); // Загрузка анимации в переменную
+        buttonApplication = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.button_animation);
+
+        logoImage = findViewById(R.id.logoView);
+        buttonAnimation = findViewById(R.id.buttonView);
+        //*Запуск анимации*//*
+        logoImage.startAnimation(logoApplication);
+        buttonAnimation.startAnimation(buttonApplication);
 
     }
 
