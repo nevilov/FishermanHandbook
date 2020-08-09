2064978475039e9eaf427bb8f8c235f3442b9950
Alim Nevilov <52937912+nevilov@users.noreply.github.com>
2020-08-09 13:02:42 +0300

    V-0.6

--- app/src/main/java/com/example/fisherhanbook/StartActivity.java
+++ app/src/main/java/com/example/fisherhanbook/StartActivity.java
@@ -1,6 +1,7 @@
 package com.example.fisherhanbook;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -11,33 +12,27 @@ import android.widget.ImageView;
 import androidx.annotation.Nullable;
 
 public class StartActivity extends Activity {
-    private Animation logoApplication, buttonApplication;
-    private Button buttonAnimation;
+    private Animation logoApplication;
     private ImageView logoImage;
+    private Typeface startface;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
         init();
+        StartMainActivity();
 
     }
 
     private void init(){
-        logoApplication = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.logo_animation); // Загрузка анимации в переменную
-        buttonApplication = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.button_animation);
+        logoApplication = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.alpha_animation); // Загрузка анимации в переменную
 
         logoImage = findViewById(R.id.logoView);
-        buttonAnimation = findViewById(R.id.buttonView);
         //*Запуск анимации*//*
         logoImage.startAnimation(logoApplication);
-        buttonAnimation.startAnimation(buttonApplication);
 
-    }
 
-    public void onClickStart(View view) {
-        Intent transition = new Intent(StartActivity.this, MainActivity.class);
-        startActivity(transition);
     }
 
     @Override
@@ -45,4 +40,21 @@ public class StartActivity extends Activity {
         super.onDestroy();
         finish();
     }
+
+    private void StartMainActivity(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1500);
+                    Intent transition = new Intent(StartActivity.this, MainActivity.class);
+                    startActivity(transition);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+
 }
